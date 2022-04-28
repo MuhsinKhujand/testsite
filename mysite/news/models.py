407247a8d542948f67b9d228c8bfde5f7cf08808
MuhsinKhujand <m7quest@mail.ru>
2022-04-28 13:34:30 +0500

    11-course

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -7,6 +7,13 @@ class News(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%M/', width_field=400, height_field=400, verbose_name="photo")
+    url_height = models.PositiveIntegerField()
+    url_width = models.PositiveIntegerField()
+    photo = models.ImageField(upload_to='photos/%Y/%M/', width_field='url_width', height_field='url_height', verbose_name="photo")
     is_published = models.BooleanField(default=True)
     email = models.EmailField(null=True)
+
+
+    def __str__(self):
+        return self.title
+
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
 def index(request):
-    return HttpResponse('fff')
+    news = News.objects.order_by('-created_at')
+    context={
+        'news': news,
+        'title': 'news list',
+
+    }
+    return render(request, 'news/index.html', context=context)
+
+
 
 def test(request):
-    return HttpResponse("<h1>dfdfdfd</h1>")
+    return HttpResponse("<h1>daddr</h1>")
