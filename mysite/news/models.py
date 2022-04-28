a9352babd8db48eaad5c341841c09d6091e6c6d5
MuhsinKhujand <m7quest@mail.ru>
2022-04-28 16:09:46 +0500

    13-course svazi modeli

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -13,6 +13,7 @@ class News(models.Model):
                               verbose_name="foto", blank=True)
     is_published = models.BooleanField(default=True, verbose_name='opublikovan')
     email = models.EmailField(null=True, verbose_name='e-pochta')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="kategoriya",)
 
     def __str__(self):
         return self.title
@@ -20,4 +21,16 @@ class News(models.Model):
     class Meta:
         verbose_name = 'Novost'
         verbose_name_plural = 'Novosti'
-        ordering= ['-created_at']
+        ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name='nazvanie kategorii', db_index=True, )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'kategoriya'
+        verbose_name_plural = 'kategoriyii'
+        ordering = ['title']
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -3,16 +3,12 @@ from django.shortcuts import render
 
 from .models import *
 
-# Create your views here.
+
 def index(request):
     news = News.objects.all()
-    context={
+    context = {
         'news': news,
         'title': 'news list',
 
     }
     return render(request, 'news/index.html', context=context)
-
-
-
-
